4c6e59f13f028ad605ee3a7d54f36bc7363d2213
murftech <murftech7@gmail.com>
2026-07-31 17:36:54 +0800

    remove install -e packages

--- startup.sh
+++ startup.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
+set -e
+
 # gh auth login 
 # gh repo list --limit 100
-# gh repo clone murftech/my-massive-app prod-finance
-
-# run this
-# chmod +x startup.sh
+# gh repo clone https
 
 cd "$(dirname "$0")"
 while [ ! -d .git ] && [ "$PWD" != "/" ]; do cd ..; done
-
 
 # git checkout qa
 # rm -rf venv
 python3.13 -m venv venv
 source venv/bin/activate
 pip install -r requirements_dev_commons.txt
-pip install -e /Users/murftech/Root/production/packages
 pip install --upgrade pip
