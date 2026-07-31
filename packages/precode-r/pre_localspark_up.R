d28089c84b6fb6c01cfedfa9026b58e9756133ab
murftech <murftech7@gmail.com>
2026-07-31 17:32:38 +0800

    apply allowlist .gitignore

--- packages/precode-r/pre_localspark_up.R
+++
@@ -1,8 +0,0 @@
-if (!exists('sc')) {
-  require(sparklyr)
-  sc <<- sparklyr::spark_connect(master = "local")}
-
-
-if (env=='prod') {hive <- '/Users/murftech/Root/hive'}
-if (env=='qa') {hive <- '/Users/murftech/Root/hive_qa'}
-sprintf('hive root anchored: %s', hive) %>% print()
